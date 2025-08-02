9de125d665ed34a70d18b77238f77d9c8e9e0f70
Sarthak-project <sarthak.0111al221170@technocratsgroup.edu.in>
2025-08-02 10:50:15 +0530

    New files added

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Bhopal">PG in Bhopal</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Indore">PG in Indore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Gwalior">PG in Gwalior</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Pune">PG in Pune</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2024 Copyright Stay.inn </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
